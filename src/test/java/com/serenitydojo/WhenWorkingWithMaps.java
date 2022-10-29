a04463251318c8007ef9a94e50db716963b581c1
PutAgung <uthaagung@gmail.com>
2022-10-29 21:11:58 +0700

    exercise 9

--- src/test/java/com/serenitydojo/WhenWorkingWithMaps.java
+++ src/test/java/com/serenitydojo/WhenWorkingWithMaps.java
@@ -3,6 +3,7 @@ package com.serenitydojo;
 import com.serenitydojo.model.FoodType;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.serenitydojo.model.FoodType.*;
@@ -22,7 +23,11 @@ public class WhenWorkingWithMaps {
     // Maps let you associate a value with some other value
     @Test
     public void creatingANewSet() {
-        Map<String, String> countryCapitals = null;
+        Map<String, String> countryCapitals = new HashMap<>();
+        countryCapitals.put("UK", "London");
+        countryCapitals.put("France", "Paris");
+        countryCapitals.put("Germany", "Berlin");
+
         // TODO: Create a new map and add the following associations:
         //  UK -> London
         //  France -> Paris
@@ -36,7 +41,11 @@ public class WhenWorkingWithMaps {
     // maps can work with different types, e.g. enums to strings
     @Test
     public void mapWithDifferentTypes() {
-        Map<String, FoodType> favoriteFood = null;
+        Map<String, FoodType> favoriteFood = new HashMap<>();
+        favoriteFood.put("cat", TUNA);
+        favoriteFood.put("dog", DELUXE_DOG_FOOD);
+        favoriteFood.put("hamster", LETTUCE);
+
         // TODO: Create a new map and add the following associations:
         //  "cat" -> TUNA
         //  "dog" -> DELUXE_DOG_FOOD
@@ -50,7 +59,11 @@ public class WhenWorkingWithMaps {
     // We can create a map more easily with the Map.of() method
     @Test
     public void usingMapOf() {
-        Map<String, FoodType> favoriteFood = null;
+        Map<String, FoodType> favoriteFood = Map.of(
+                "cat", TUNA,
+                "dog", DELUXE_DOG_FOOD,
+                "hamster", LETTUCE
+        );
         // TODO: Create a new map and add the following associations using Map.of()
         //  "cat" -> TUNA
         //  "dog" -> DELUXE_DOG_FOOD
@@ -71,9 +84,11 @@ public class WhenWorkingWithMaps {
                 "hamster", LETTUCE);
 
         // TODO: Check that the map contains a key of "dog"
-        boolean containsDog = false;
+        //boolean containsDog = false;
 
-        assertThat(containsDog).isTrue();
+        favoriteFood.containsKey("dog");
+        //System.out.println("is present "+favoriteFood.containsKey("cat"));
+        //assertThat(containsDog).isTrue();
     }
 
     // We can check that a map contains a specified value
@@ -85,8 +100,8 @@ public class WhenWorkingWithMaps {
                 "hamster", LETTUCE);
 
         // TODO: Check that the map contains a value of TUNA
-        boolean containsTuna = false;
-
-        assertThat(containsTuna).isTrue();
+        //boolean containsTuna = false;
+        favoriteFood.containsValue(TUNA);
+        //assertThat(containsTuna).isTrue();
     }
 }
